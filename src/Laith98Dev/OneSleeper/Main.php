ba803da289a96344f9d1cac2ed942658861575cc
Laith Youtuber <49840784+Laith98Dev@users.noreply.github.com>
2021-06-12 10:51:25 -0700

    Create Main.php

---
+++ src/Laith98Dev/OneSleeper/Main.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Laith98Dev\OneSleeper;
+
+/*  
+ *  A plugin for PocketMine-MP.
+ *  
+ *	 _           _ _   _    ___   ___  _____             
+ *	| |         (_) | | |  / _ \ / _ \|  __ \            
+ *	| |     __ _ _| |_| |_| (_) | (_) | |  | | _____   __
+ *	| |    / _` | | __| '_ \__, |> _ <| |  | |/ _ \ \ / /
+ *	| |___| (_| | | |_| | | |/ /| (_) | |__| |  __/\ V / 
+ *	|______\__,_|_|\__|_| |_/_/  \___/|_____/ \___| \_/  
+ *	
+ *  Copyright (C) 2021 Laith98Dev
+ *  
+ *	Youtube: Laith Youtuber
+ *	Discord: Laith98Dev#0695
+ *	Gihhub: Laith98Dev
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 	
+ */
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\level\Level;
+use pocketmine\scheduler\Task;
+use pocketmine\event\player\PlayerBedEnterEvent;
+
+class Main extends PluginBase implements Listener {
+	
+	public function onEnable() {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	
+	public function onBedEnter(PlayerBedEnterEvent $event){
+		$player = $event->getPlayer();
+		$this->getScheduler()->scheduleRepeatingTask(new SleepTask($this, $player->getLevel()), 20 * 3);
+	}
+}
